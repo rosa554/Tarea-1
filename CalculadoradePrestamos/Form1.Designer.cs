2155ab51859168897c5d550035a80a2741e4197a
rosa554 <rosaolivo52@gmail.com>
2017-07-25 00:42:42 -0600

    Primera tarea de programacion 3

---
+++ CalculadoradePrestamos/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace CalculadoradePrestamos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nmAnios = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrestamo = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textInteres = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nmAnios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nmAnios
+            // 
+            this.nmAnios.Location = new System.Drawing.Point(213, 115);
+            this.nmAnios.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nmAnios.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nmAnios.Name = "nmAnios";
+            this.nmAnios.Size = new System.Drawing.Size(118, 20);
+            this.nmAnios.TabIndex = 0;
+            this.nmAnios.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.nmAnios.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nmAnios.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Valor del Prestamo:";
+            // 
+            // txtPrestamo
+            // 
+            this.txtPrestamo.Location = new System.Drawing.Point(213, 29);
+            this.txtPrestamo.Name = "txtPrestamo";
+            this.txtPrestamo.Size = new System.Drawing.Size(118, 20);
+            this.txtPrestamo.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txtPrestamo, "Digite el valor del prestamo");
+            this.txtPrestamo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 100;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // textInteres
+            // 
+            this.textInteres.Location = new System.Drawing.Point(213, 74);
+            this.textInteres.Name = "textInteres";
+            this.textInteres.Size = new System.Drawing.Size(118, 20);
+            this.textInteres.TabIndex = 4;
+            this.toolTip2.SetToolTip(this.textInteres, "Digite la tasa de porcentaje");
+            this.toolTip1.SetToolTip(this.textInteres, "Digite el valor del prestamo");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tasa de porcentaje anual:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Plazo:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtResultado.Location = new System.Drawing.Point(43, 186);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(266, 68);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcular.Location = new System.Drawing.Point(137, 262);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // toolTip2
+            // 
+            this.toolTip2.IsBalloon = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(343, 312);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textInteres);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrestamo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nmAnios);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Prestamos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nmAnios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nmAnios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrestamo;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textInteres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
---
+++ CalculadoradePrestamos/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoradePrestamos
+{
+    public partial class Form1 : Form
+    {
+        private object txtInteres;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Double vrPrestamo, vrInteres, anios, monto;
+            string cadena = "";
+
+            vrPrestamo = Convert.ToDouble(txtPrestamo.Text);
+            vrInteres = Convert.ToDouble(txtInteres);
+            anios = Convert.ToDouble(nmAnios.Value);
+
+            for (int i = 1; i <= anios; i++)
+            {
+                monto = (vrPrestamo * (1 + (vrInteres / 100))) * i;
+                cadena += (i + "\t" + string.Format("{0:c}", monto + "\r\n"));
+            }
+            txtResultado.Text = cadena;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
